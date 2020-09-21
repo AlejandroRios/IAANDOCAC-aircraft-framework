corrected units of baseline aircraft to match dimensionality analysis

aircraft_framework_win/framework_PhD/framework/Performance/balanced_field_length.py
@@ -1,16 +1,16 @@
 """
-Function  : balanced_length_field.py
-Title     :
-Written by: 
-Date      : 
-Last edit :
+Function  : balanced_fiel_length.py
+Title     : Balanced field length function
+Written by: Alejandro Rios
+Date      : September/2020
+Last edit : September/2020
 Language  : Python
 Aeronautical Institute of Technology - Airbus Brazil
 
 Description:
     - Balanced length field function 
-    - Reference: Torenbeek. 1982
-    - Chapter 5, page 169, equation 5-91
+    - Reference: Torenbeek. 1982 and Gudmunsson 2014
+    - Chapter 5, page 169, equation 5-91 and Chapter 17 equation 17-1
 Inputs:
     - aicraft data
     - airport data
@@ -33,29 +33,28 @@ import numpy as np
 ########################################################################################
 """FUNCTIONS"""
 ########################################################################################
-
-def balanced_lenght_field(aircraft_data,airport_data):
+def balanced_field_length(aircraft_data,airport_data):
     '''
     Note: for project design the case of delta_gamma2 = 0 presents most interest, as the corresponding weight
     is limited by the second segment climb requirement (Torenbeek, 1982) 
     '''
     # Aircraft data import
-    CL_max_takeoff = aircraft_data['CL_maximum_takeoff']
-    weight_takeoff = aircraft_data['maximum_takeoff_weight']
-    wing_surface = aircraft_data['wing_surface']
-    T_avg = aircraft_data['thrust_average']
+    CL_max_takeoff = aircraft_data['CL_maximum_takeoff'] 
+    weight_takeoff = aircraft_data['maximum_takeoff_weight'] # [N]
+    wing_surface = aircraft_data['wing_surface'] # [m2]
+    T_avg = aircraft_data['thrust_average'] # [N]
     
     # Airport data import
-    airfield_elevation = airport_data['elevation']
-    delta_ISA = airport_data['delta_ISA']
+    airfield_elevation = airport_data['elevation'] # [m]
+    delta_ISA = airport_data['delta_ISA'] # [deg C]
     
     h_takeoff = 10.7 # horizontal distance from airfield surface requirement according to FAR 25 - [m] 
-    delta_gamma2 = 0
-    delta_S_takeoff = 200 
-    g = 9.81
+    delta_gamma2 = 0.024
+    delta_S_takeoff = 200 # [m]
+    g = 9.807 # [m/s2] 
     CL_2 = 0.694*CL_max_takeoff 
     mu = 0.01*CL_max_takeoff + 0.02
-    _,_,sigma,_,_,rho,_ = atmosphere_ISA_deviation(airfield_elevation,delta_ISA)
+    _,_,sigma,_,_,rho,_ = atmosphere_ISA_deviation(airfield_elevation,delta_ISA) # [kg/m3]
    
     aux1 = 0.863/(1 + (2.3*delta_gamma2))
     aux2 = ((weight_takeoff/wing_surface)/(rho*g*CL_2)) + h_takeoff
@@ -70,7 +69,7 @@ def balanced_lenght_field(aircraft_data,airport_data):
 
     aux4 = delta_S_takeoff/np.sqrt(sigma)
 
-    return aux1*aux2*aux3 + aux4
+    return aux1*aux2*aux3 + aux4 # [m]
 ########################################################################################
 """MAIN"""
 ########################################################################################